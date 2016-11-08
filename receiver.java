ac5f9a2822a47d607581bad1cf35ee5128987000
aditi741997 <aditi741997@gmail.com>
2016-11-08 18:00:27 +0530

    yo

--- receiver.java
+++ receiver.java
@@ -5,10 +5,29 @@ import java.io.*;
 
 class receiver
 {
-	private int ack_number;
+	private int Ack_number;
+	int Port;
+
+	receiver(int port)
+	{
+		Port = port;
+		Ack_number = 0;
+	}
+
+	void updateAck()
+	{
+        byte[] receive_buff = new byte[1300];
+        DatagramPacket rec_pkt = new DatagramPacket(receive_buff,receive_buff.length);
+		DatagramSocket rec = new DatagramSocket(Port);
+
+		while (true)
+		{
+			// receive, send back an ACK pkt
+		}
+	}
 	
 	public static void main(String[] args)
 	{
-		
+		r.updateAck();
 	}
 }
